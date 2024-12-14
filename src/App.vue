e6fc84e4564b8893e2ff540b1c1fbc7ae70531af
Marcos <marcosjfr3@gmail.com>
2024-12-14 18:08:24 -0400

    checklist added

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
   import AppHeader from './components/AppHeader.vue';
+  import ToDoList from './components/ToDoList.vue';
 </script>
 
 <template>
   <AppHeader />
+  <ToDoList />
 </template>
 
 <style scoped>
---
+++ src/components/ToDoList.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+    import ListItem from './ListItem.vue';
+</script>
+
+<template>
+    <ul>
+        <ListItem :is-Checked="false">
+            Esta es la tarea
+        </ListItem>
+    </ul>
+</template>
+
+<style>
+
+</style>
